e3418bc75928f787930839b79962ab5f7851ffc6
Shivang Mishra <shivang978@gmail.com>
2024-06-20 18:54:00 +0530

    initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
+import path, { resolve } from "path";
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react'
+import {nodePolyfills} from "vite-plugin-node-polyfills";
+import dts from 'vite-plugin-dts';
+import {globSync} from 'glob';
+import { fileURLToPath } from "node:url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [dts(), react(), nodePolyfills()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/main.ts'),
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'react/jsx-runtime'],
+            input: {
+              main: resolve(__dirname, 'src/main.ts'),
+            },
+            output: {
+              entryFileNames: '[name].js',
+              assetFileNames: 'assets/[name][extname]',
+              globals: {
+                react: 'React',
+                'react-dom': 'React-dom',
+              },
+            },
+          },
+    },
+});
